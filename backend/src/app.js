0bc5377000f693693294e2368b8b5ba235c05bea
Daniel Wojcickoski <60068389+danielwojcickoski@users.noreply.github.com>
2020-07-13 19:45:53 -0300

    Teste for migrate in heroku

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,11 +9,8 @@ const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
 app.use(routes);
-
 
 module.exports = app;
