4f403189b5fc6c9ca891afad3a7503201fa71115
Daniel Wojcickoski <60068389+danielwojcickoski@users.noreply.github.com>
2020-07-15 13:08:23 -0300

    Revert "Test for heroku"
    This reverts commit 8c05d6c45f76b5c669032aaaa56b15d93a28fb57.

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,6 @@
 // Update with your config settings.
 const path = require('path');
-const envPath = path.join(__dirname, '.env')
+const envPath = path.join(__dirname, 'backend', '.env')
 require('dotenv').config({
     path: envPath
 });
@@ -9,12 +9,12 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './src/database/dbDev.sqlite'
+      filename: './backend/src/database/dbDev.sqlite'
     },
     migrations: {
-			directory: './src/database/migrations'
+			directory: './backend/src/database/migrations'
 		},
-		useNullAsDefault: false,
+		useNullAsDefault: true,
   },
 
   production: {
@@ -23,7 +23,7 @@ module.exports = {
       filename: './backend/src/database/db.sqlite'
     },
     migrations: {
-			directory: './src/database/migrations'
+			directory: './backend/src/database/migrations'
 		},
 		useNullAsDefault: true,
   }
