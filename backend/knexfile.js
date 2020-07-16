7a1a51156f48000d43b3bb30f272c8468945ebe0
Daniel Wojcickoski <60068389+danielwojcickoski@users.noreply.github.com>
2020-07-15 21:55:22 -0300

    config postgres in production

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -9,23 +9,20 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './src/database/dbDev.sqlite'
+      filename: `${__dirname}/src/database/db.sqlite`
     },
     migrations: {
-			directory: './src/database/migrations/'
+			directory: `${__dirname}/src/database/migrations/`
 		},
 		useNullAsDefault: true,
   },
 
   production: {
-    client: 'sqlite3',
-    connection: {
-      filename: './src/database/db.sqlite'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
-			directory: './src/database/migrations/'
+			directory: `${__dirname}/src/database/migrations/`
 		},
-		useNullAsDefault: true,
   }
 
 };
