ad5d5d767cbd100461144c42889dc2c82561db00
Daniel Wojcickoski <60068389+danielwojcickoski@users.noreply.github.com>
2020-07-15 22:05:36 -0300

    Solve baseUrl frontend error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://eifchar.herokuapp.com/3333' : 'http://localhost:3333',
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://eifchar.herokuapp.com' : 'http://localhost:3333',
 })
 
 export default api;
